2fc1c66e19df10d293d50fe8ec5f9895db6c7c59
Andy Wahyudi <andy.wahyudi@rschooltoday.com>
2022-11-21 09:42:07 +0700

    github workflows

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kaisarjailani.github.io//",
+  site: "https://kaisarjailani.github.io",
   integrations: [
     tailwind({
       config: {
